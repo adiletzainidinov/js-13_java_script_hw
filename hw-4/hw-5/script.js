a9d8b7a79f984531d2249e73bb7a4268be469a99
Adilet Zainidinov <adiletzainidinov22@gmail.com>
2024-02-24 17:52:11 +0600

    hw-5

---
+++ hw-4/hw-5/script.js
@@ -0,0 +1,68 @@
+// function showGreetings(greetings, name){
+//     alert(`${greetings} ${name}`)
+// }
+
+// showGreetings("Привет", "Мир");
+// showGreetings("Привет", "Адилет");
+
+// function calcNumber(calc1, calc2){
+//     result = calc1 + calc2
+//     console.log(result);
+// }
+
+// calcNumber(23, 32);
+// calcNumber(232, 323);
+
+// function calculateCvadrat(num){
+//     num = Math.pow(num,2)
+//     console.log(num);
+// }
+
+// calculateCvadrat(4);
+// calculateCvadrat(9);
+
+// function namePeople(namePeople) {
+//     alert(`Ассаламу алайкум ${namePeople}`)
+// }
+
+// namePeople('Adilet')
+
+// function string(firstString, secondString){
+//     result = firstString + ' ' + secondString
+//     console.log(result);
+// }
+
+// string('This is first line string.', 'This is second line string.');
+
+// function calcExamination(numbers){
+// if (numbers % 2 === 0) {
+//     console.log(numbers + ' Число четное');
+// }    else {
+//     console.log(numbers + ' Число нечетное');
+// }
+// }
+
+// calcExamination(21);
+// calcExamination(18);
+
+// function calculate(num1, num2){
+//     if (num1 > num2) {
+//         console.log(num1 + ' ' + 'This is num1');
+//     }else {
+//         console.log(num2 + ' ' + 'This is num2');
+//     }
+// }
+
+// calculate(73, 43);
+
+// function stringLength(lengths){
+//     let proverka = lengths
+//     console.log(`Слово ${proverka} занимает ${proverka.length} длину`);
+// }
+
+// stringLength('This is string Length');
+
+
+
+
+
