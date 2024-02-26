6f09beb1bc45215de46340803412495f4bc4c6b8
Adilet Zainidinov <adiletzainidinov22@gmail.com>
2024-02-26 18:03:56 +0600

    hw-6

---
+++ hw-6/script.js
@@ -0,0 +1,94 @@
+// let car = {
+//     brand:  "Bugati",
+//     model: "Shiron",
+//     year: "2017",
+// }
+
+// console.log(car);
+// console.log(car.year);
+
+// car.year = "2018";
+
+// console.log(car.year);
+
+// car.color = "red"
+
+// console.log(car.color);
+
+// delete car.model;
+
+// console.log(car);
+
+// car = displayInfo;
+
+// function displayInfo(car){
+//     return car = 'Car';
+// };
+
+
+// console.log(displayInfo(car));
+
+// 6-Задача наверху
+
+// const person = {
+//     name: "Adilet",
+//     address: {
+//         sity: "Bishkek",
+//         address: {
+//             area: "Alamydun",
+//         village: "Tendik"
+//         }
+//     }
+// }
+
+// console.log(person);
+
+
+// const school = {
+// name: "32-средняя школа",
+// class: "1-11класс"
+// }
+
+// const work = {
+//     job: "cook",
+//     kitchen: "evropa"
+// }
+
+// console.log(school === work);
+
+// car = {
+//     model: "bugati"
+// }
+
+// car.color = "black"
+
+// console.log(car);
+
+// const peacksoft = {
+//     js: "object",
+//     html: "p",
+//     css: "syle"
+// };
+
+
+// let newpeacksoft = Object.assign({},peacksoft)
+// console.log(newpeacksoft);
+// console.log(newpeacksoft.html);
+
+// const student = (name,age) =>{
+//     name = {
+//         name: "Adilet",
+//         surname: "Zainidinov"
+//     }
+//     age = {
+//         age: "24",
+//         "Yer of birth": "1999"
+//     }
+//     console.log(`name: ${name.name} age: ${age.age}`);
+// }
+
+// student();
+
+
+
+
